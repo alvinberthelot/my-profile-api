53010295f8104e6ea5e9a0edf67d0c84e9b386d1
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2020-05-10 16:43:20 +0200

    chore: Split website data in 2 functions

--- src/website.js
+++ src/website.js
@@ -1,34 +1,8 @@
 const { getVersion } = require("./contents/content-version")
-const { getSkills } = require("./contents/content-skills")
-const { getTechnicalSkills } = require("./contents/content-technical-skills")
-const { getEducations } = require("./contents/content-educations")
-const { getWorkExperiences } = require("./contents/content-work-experiences")
-const { getEvents } = require("./contents/content-events")
 
 exports.handler = (event, context, callback) => {
   content = {
     version: getVersion(),
-    firstname: "Alvin",
-    lastname: "Berthelot",
-    skillsSection: getSkills(),
-    technicalSkillsSection: {
-      title: "Environnements techniques",
-      description:
-        "Des connaissances larges sur les principales technologies autour de l'industrialisation web.",
-      aggregates: getTechnicalSkills(),
-    },
-    educationSection: {
-      title: "Formations",
-      description:
-        "Mon objectif c'est de faire au minimum 2 formations par an.",
-      educations: getEducations(),
-    },
-    workExperienceSection: {
-      title: "Expériences",
-      description: "",
-      experiences: getWorkExperiences(),
-    },
-    eventsSection: getEvents(),
   }
   callback(null, {
     statusCode: 200,
