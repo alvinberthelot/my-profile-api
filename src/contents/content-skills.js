aaff0c6241171a4b3df4c6e78920d8afbd49aa9a
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2020-05-11 15:50:06 +0200

    feat: Init content about skills

--- src/contents/content-skills.js
+++ src/contents/content-skills.js
@@ -5,11 +5,26 @@ const getContent = () => {
       "Je propose des services dans toutes les étapes clés pour réaliser des applications web et mobiles de qualité.",
     skills: [
       {
-        title: "Tech lead / développeur senior",
+        title: "Tech lead / développeur sénior",
         subskills: [
-          "Encadrement technique",
-          "Développement front-end",
-          "Développement back-end",
+          "Encadrement technique et développement front-end (HTML, CSS, JavaScript)",
+          "Développement back-end (API, Node.js, Java EE)",
+          "Développement mobile (natif Android et applications hybrides)",
+        ],
+      },
+      {
+        title: "Consultant",
+        subskills: [
+          "Conseil sur les choix technologiques et fonctionnels pour la réalisation d'applications web et mobiles",
+          "Facilitation et mise en place d'ateliers de conception",
+        ],
+      },
+      {
+        title: "Chef de projet technique",
+        subskills: [
+          "Rédaction de spécifications techniques ou fonctionnelles",
+          "Encadrement et animation d'une équipe de développeurs",
+          "Communication et priorisation des différentes phases",
         ],
       },
     ],
